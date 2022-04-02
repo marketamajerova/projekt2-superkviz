231724568f9648ddd508a987a39846dfb6742038
Marketa Majerova <marketamajerova.6@gmail.com>
2022-04-02 23:56:20 +0200

    Creating questions and answers

--- kviz.js
+++ kviz.js
@@ -1,1 +1,20 @@
-console.log('jsem na linkovan');
+let otazka1 = {
+    otazka: "Až o kolik centimetrů může 'vyrůst' Eiffelova věž v létě pod vlivem změny teplotních podmínek?",
+    obrazek: 'obrazky/vez.jpeg',
+    odpovedi: ['100m','10cm','15cm'],
+    indexSpravneOdpovedi: 3
+}
+
+let otazka2 = {
+    otazka: 'Ze své biologické podstaty avocado je... ?',
+    obrazek: 'obrazky/avocado.jpeg',
+    odpovedi: ['ovoce','zelenina','pečivo'],
+    indexSpravneOdpovedi: 1
+}
+
+let otazka3 = {
+    otazka: 'V jakém roce vzlétlo první motorové letadlo?',
+    obrazek: 'obrazky/letadlo.jpeg',
+    odpovedi: ['1903', '1920', '1941'],
+    indexSpravneOdpovedi: 1
+}

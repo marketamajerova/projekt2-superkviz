29db3e1fd2d3bd19ee0f3049071d29bedac039a3
Marketa Majerova <marketamajerova.6@gmail.com>
2022-04-06 21:42:11 +0200

    [STRUCTURE CHANGE] putting all question in one aaray

--- kviz.js
+++ kviz.js
@@ -1,23 +1,22 @@
-let otazka1 = {
-    otazka: "Až o kolik centimetrů může 'vyrůst' Eiffelova věž v létě pod vlivem změny teplotních podmínek?",
-    obrazek: 'obrazky/vez.png',
-    odpovedi: ['100cm','10cm','15cm'],
-    indexSpravneOdpovedi: 3
-}
-
-let otazka2 = {
-    otazka: 'Ze své biologické podstaty avocado je... ?',
-    obrazek: 'obrazky/avocado.jpeg',
-    odpovedi: ['ovoce','zelenina','pečivo'],
-    indexSpravneOdpovedi: 1
-}
-
-let otazka3 = {
-    otazka: 'V jakém roce vzlétlo první motorové letadlo?',
-    obrazek: 'obrazky/letadlo.png',
-    odpovedi: ['1903', '1920', '1941'],
-    indexSpravneOdpovedi: 1
-}
+let poleOtazek = [
+    {
+        otazka: "Až o kolik centimetrů může 'vyrůst' Eiffelova věž v létě pod vlivem změny teplotních podmínek?",
+        obrazek: 'obrazky/vez.png',
+        odpovedi: ['100cm','10cm','15cm','1cm'],
+        indexSpravneOdpovedi: 3
+    },
+    {
+        otazka: 'Ze své biologické podstaty avocado je... ?',
+        obrazek: 'obrazky/avocado.jpeg',
+        odpovedi: ['ovoce','zelenina'],
+        indexSpravneOdpovedi: 1
+    },
+    {
+        otazka: 'V jakém roce vzlétlo první motorové letadlo?',
+        obrazek: 'obrazky/letadlo.png',
+        odpovedi: ['1903', '1920', '1941'],
+        indexSpravneOdpovedi: 1
+    }]
 
 let otazka = document.getElementById('otazka');
 let obrazek = document.getElementById('obrazek');
@@ -30,12 +29,12 @@ zobrazitOtazku1();
 //dale jen funkce
 
 function zobrazitOtazku1(){ 
-    otazka.innerText = otazka1.otazka;
-    obrazek.src = otazka1.obrazek;
+    otazka.innerText = poleOtazek[0].otazka;
+    obrazek.src = poleOtazek[0].obrazek;
 
-    otazka1.odpovedi.forEach(function(odpoved, indx){
+    poleOtazek[0].odpovedi.forEach(function(odpoved, indx){
         odpoved = document.createElement('li');
-        odpoved.innerText = otazka1.odpovedi[indx];
+        odpoved.innerText = poleOtazek[0].odpovedi[indx];
         odpovedi.appendChild(odpoved);
     })
 
@@ -54,11 +53,11 @@ function zobrazitOtazku1(){
 function zobrazitOtazku2(){ 
     odpovedi.innerText = '';
 
-    otazka.innerText = otazka2.otazka;
-    obrazek.src = otazka2.obrazek;
-    otazka2.odpovedi.forEach(function(odpoved, indx){
+    otazka.innerText = poleOtazek[1].otazka;
+    obrazek.src = poleOtazek[1].obrazek;
+    poleOtazek[1].odpovedi.forEach(function(odpoved, indx){
         odpoved = document.createElement('li');
-        odpoved.innerText = otazka2.odpovedi[indx];
+        odpoved.innerText = poleOtazek[1].odpovedi[indx];
         odpovedi.appendChild(odpoved);
     })
     poradi.innerText = "Otázka 2/3"
@@ -70,11 +69,11 @@ function zobrazitOtazku2(){
 function zobrazitOtazku3(){ 
     odpovedi.innerText = '';
 
-    otazka.innerText = otazka3.otazka;
-    obrazek.src = otazka3.obrazek;
-    otazka3.odpovedi.forEach(function(odpoved, indx){
+    otazka.innerText = poleOtazek[2].otazka;
+    obrazek.src = poleOtazek[2].obrazek;
+    poleOtazek[2].odpovedi.forEach(function(odpoved, indx){
         odpoved = document.createElement('li');
-        odpoved.innerText = otazka3.odpovedi[indx];
+        odpoved.innerText = poleOtazek[2].odpovedi[indx];
         odpovedi.appendChild(odpoved);
     })
     poradi.innerText = "Otázka 3/3"

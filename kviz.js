4de63cf1acec0a61a7c4532951d790e69ef22458
Marketa Majerova <marketamajerova.6@gmail.com>
2022-04-10 00:51:19 +0200

    [FEATURE] Presenting score

--- kviz.js
+++ kviz.js
@@ -3,26 +3,29 @@ let poleOtazek = [
         otazka: "Až o kolik centimetrů může 'vyrůst' Eiffelova věž v létě pod vlivem změny teplotních podmínek?",
         obrazek: 'obrazky/vez.png',
         odpovedi: ['100cm','10cm','15cm','1cm'],
-        indexSpravneOdpovedi: 3
+        indexSpravneOdpovedi: 2
     },
     {
         otazka: 'Ze své biologické podstaty avocado je... ?',
         obrazek: 'obrazky/avocado.jpeg',
         odpovedi: ['ovoce','zelenina'],
-        indexSpravneOdpovedi: 1
+        indexSpravneOdpovedi: 0
     },
     {
         otazka: 'V jakém roce vzlétlo první motorové letadlo?',
         obrazek: 'obrazky/letadlo.png',
         odpovedi: ['1903', '1920', '1941'],
-        indexSpravneOdpovedi: 1
+        indexSpravneOdpovedi: 0
     }]
+
+// console.log(poleOtazek.od)
 
 let otazka = document.getElementById('otazka');
 let obrazek = document.getElementById('obrazek');
 let odpovedi = document.getElementById('odpovedi');
 let poradi = document.getElementById('poradi');
 let pocitadlo = document.getElementById('pocitadlo');
+let vybraneOdpovedi = document.getElementById('vybraneOdpovedi');
 
 zobrazitOtazku1();
 
@@ -41,11 +44,13 @@ function zobrazitOtazku1(){
 
     odpovedi.onclick = function(event){
         let zvolenaOdpoved = event.srcElement.innerHTML;
-        console.log(zvolenaOdpoved);
+        //console.log(zvolenaOdpoved);
         if (zvolenaOdpoved == "15cm"){
             pocitadlo.dataset.celkem = parseInt(pocitadlo.dataset.celkem) + 1;
-            console.log(pocitadlo.dataset.celkem);
+            //console.log(pocitadlo.dataset.celkem);
         }
+        vybraneOdpovedi.dataset.prvniOdpoved = zvolenaOdpoved;
+        
         zobrazitOtazku2();
     }
 }
@@ -65,12 +70,12 @@ function zobrazitOtazku2(){
     
     odpovedi.onclick = function(event){
         let zvolenaOdpoved = event.srcElement.innerHTML;
-        console.log(zvolenaOdpoved);
+        // console.log(zvolenaOdpoved);
         if (zvolenaOdpoved == "ovoce"){
             pocitadlo.dataset.celkem = parseInt(pocitadlo.dataset.celkem) + 1;
-            console.log(pocitadlo.dataset.celkem);
+           // console.log(pocitadlo.dataset.celkem);
         }
-
+        vybraneOdpovedi.dataset.druhaOdpoved = zvolenaOdpoved;
         zobrazitOtazku3();
     }
 }
@@ -89,11 +94,12 @@ function zobrazitOtazku3(){
 
     odpovedi.onclick = function(event){
         let zvolenaOdpoved = event.srcElement.innerHTML;
-        console.log(zvolenaOdpoved);
+        //console.log(zvolenaOdpoved);
         if (zvolenaOdpoved == "1903"){
             pocitadlo.dataset.celkem = parseInt(pocitadlo.dataset.celkem) + 1;
-            console.log(pocitadlo.dataset.celkem);
+            // console.log(pocitadlo.dataset.celkem);
         }
+        vybraneOdpovedi.dataset.tretiOdpoved = zvolenaOdpoved;
         zobrazitHodnoceni();
     }
 }
@@ -104,9 +110,70 @@ let kviz = document.querySelector('.kviz');
 function zobrazitHodnoceni(){
     kviz.innerText = '';
     kviz.className = 'vysledek';
+
     let nadpis = document.createElement('h2');
     nadpis.innerText = "Tvoje hodnocení";
     kviz.appendChild(nadpis);
+    
+    //PRVNI OTAZKA
+    let prvniOtazka = document.createElement('h3');
+    prvniOtazka.innerText = poleOtazek[0].otazka;
+    kviz.appendChild(prvniOtazka);
+
+    let vybranaPrvni = document.createElement('div');
+    vybranaPrvni.innerText = "Tvoje odpověď: " + vybraneOdpovedi.dataset.prvniOdpoved;
+    kviz.appendChild(vybranaPrvni);
+
+    let vyhodnoceniPrvni = document.createElement('div');
+
+    if(vybraneOdpovedi.dataset.prvniOdpoved == poleOtazek[0].odpovedi[poleOtazek[0].indexSpravneOdpovedi]){
+        vyhodnoceniPrvni.innerText = "Tohle je SPRÁVNĚ.";
+    } else {
+        vyhodnoceniPrvni.innerText = "Správná odpověď: "+ poleOtazek[0].odpovedi[poleOtazek[0].indexSpravneOdpovedi];
+    }
+    kviz.appendChild(vyhodnoceniPrvni);
+
+    //DRUHA OTAZKA
+    let druhaOtazka = document.createElement('h3');
+    druhaOtazka.innerText = poleOtazek[1].otazka;
+    kviz.appendChild(druhaOtazka);
+
+    let vybranaDruha = document.createElement('div');
+    vybranaDruha.innerText = "Tvoje odpověď: " + vybraneOdpovedi.dataset.druhaOdpoved;
+    kviz.appendChild(vybranaDruha);
+
+    let vyhodnoceniDruhe = document.createElement('div');
+
+    if(vybraneOdpovedi.dataset.druhaOdpoved == poleOtazek[1].odpovedi[poleOtazek[1].indexSpravneOdpovedi]){
+        vyhodnoceniDruhe.innerText = "Tohle je SPRÁVNĚ.";
+    } else {
+        vyhodnoceniDruhe.innerText = "Správná odpověď: "+ poleOtazek[1].odpovedi[poleOtazek[1].indexSpravneOdpovedi];
+    }
+    kviz.appendChild(vyhodnoceniDruhe);
+
+    //TRETI OTAZKA
+    let tretiOtazka = document.createElement('h3');
+    tretiOtazka.innerText = poleOtazek[2].otazka;
+    kviz.appendChild(tretiOtazka);
+
+    let vybranaTreti = document.createElement('div');
+    vybranaTreti.innerText = "Tvoje odpověď: " + vybraneOdpovedi.dataset.tretiOdpoved;
+    kviz.appendChild(vybranaTreti);
+
+    let vyhodnoceniTreti = document.createElement('div');
+
+    if(vybraneOdpovedi.dataset.tretiOdpoved == poleOtazek[2].odpovedi[poleOtazek[2].indexSpravneOdpovedi]){
+        vyhodnoceniTreti.innerText = "Tohle je SPRÁVNĚ.";
+    } else {
+        vyhodnoceniTreti.innerText = "Správná odpověď: "+ poleOtazek[2].odpovedi[poleOtazek[2].indexSpravneOdpovedi];
+    }
+    kviz.appendChild(vyhodnoceniTreti);
+
+    //Souhrnne hodnoceni
+    let shrnuti = document.createElement('h2');
+    shrnuti.innerText = "Správně " + pocitadlo.dataset.celkem + " ze 3 otázek. " + "Úspěšnost: " + Math.round((pocitadlo.dataset.celkem/3)*100) + "%.";
+    kviz.appendChild(shrnuti);
+
 }
 
 
